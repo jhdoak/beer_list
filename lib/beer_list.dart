40a2be8e03693f8248316ce0f2c98566701dbac7
Justin Doak <justin@jhdoak.com>
2018-01-26 12:41:30 -0500

    Initial Commit - Generated Project

---
+++ lib/beer_list.dart
@@ -0,0 +1,11 @@
+/// beer_list
+///
+/// A Aqueduct web server.
+library beer_list;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'beer_list_sink.dart';
